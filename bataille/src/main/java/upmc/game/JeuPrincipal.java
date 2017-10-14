86bde139069f9766809a7d568e54fcda838af9bb
FefeKimi <r.felana.gyal@gmail.com>
2017-10-14 13:36:48 +0200

    Optimisation profonde de la classe JeuPrincipal (ajout de petites methodes pour éviter les méthodes trop lourdes, correction d'erreur "bêtes")

--- bataille/src/main/java/upmc/game/JeuPrincipal.java
+++ bataille/src/main/java/upmc/game/JeuPrincipal.java
@@ -7,12 +7,16 @@ public class JeuPrincipal {
     private Joueur joueur1;
     private Joueur joueur2;
     private Paquet paquet;
-
-
+    private int choix;
+    private int tour;
+    private int attack;
+            
     public JeuPrincipal() {
         /*initialisation*/
+        this.choix=0;
+        this.tour=1;
+        this.attack=0;
         System.out.println("* Jeu de la Bataille *\n");
-
          /*Saisie des noms*/
         System.out.print("Pseudo du joueur 1: ");
         System.out.flush();
@@ -41,76 +45,70 @@ public class JeuPrincipal {
             j++;
         }
     }
-
-    public Joueur getJoueur1(){
-        return this.joueur1;
+    
+    /*Cette méthode sera appelée à chaque fois que l'un des joueurs devra soit tirer une carte ou quitter le Jeu*/
+    public void actionJoueur(){
+        if(this.tour%2!=0){
+            System.out.println("C'est à "+this.joueur1.getPseudo()+" de jouer");
+        }else {
+            System.out.println("C'est à "+this.joueur2.getPseudo()+" de jouer");
+        }
+        if(this.choix!=2 && this.choix !=1 && this.choix!=0)
+            erreur("Saisie incorrecte ! ");
+        System.out.println("1. Tirer la prochaine carte ");
+        System.out.println("2. Quitter");
+        System.out.flush();
+        Scanner console = new Scanner(System.in);
+        this.choix = console.nextInt();
+        System.out.println("- -");
     }
-
-    public Joueur getJoueur2() {
-        return this.joueur2;
+    
+    /*Lancement Menu*/
+    public void menu(){ 
+        /*Tant qu'un des joueurs n'a pas décidé de quitter la partie,
+        ou que les paquets des joueurs ne sont pas vide, la partie continue*/
+        while(this.choix !=2 && this.choix!=1 && this.joueur1.getPaquet().isEmpty() ==false && this.joueur2.getPaquet().isEmpty() ==false) {
+            System.out.print(attack);
+            this.actionJoueur();            
+            this.partie();   
+        }
+       this.finDuJeu();
     }
-
-    /*Jeu de bataille*/
-    public void attaque(){
-        int choix=0;
-        int tour=1;
-        int attack=0;
+    
+    /*Début de la partie*/
+    public void partie() {
         Carte carteRetiree1 = new Carte(0,"");
         Carte carteRetiree2 = new Carte(0,"");
-
-        /*Menu Tirer une carte ou Quitter*/
-        while(choix !=2 && choix!=1 && this.joueur1.getPaquet() != null && this.joueur2.getPaquet() !=null) {
-            this.afficherScore();
-            if(tour%2!=0){
-                System.out.println("C'est à "+this.joueur1.getPseudo()+" de jouer");
+        /*On tire une carte du joueur 1 ou 2 selon le tour*/
+        if(this.choix==1) {
+            if (this.tour % 2 != 0) {
+                carteRetiree1 = this.joueur1.getPaquet().get(0);
+                this.joueur1.tirer(carteRetiree1);
+                System.out.println(carteRetiree1.toString() + "\n");
+                this.choix = 0;
+                this.attack++;
             }else {
-                System.out.println("C'est à "+this.joueur2.getPseudo()+" de jouer");
+                carteRetiree2 = this.joueur2.getPaquet().get(0);
+                this.joueur2.tirer(carteRetiree2);
+                System.out.println(carteRetiree2.toString() + "\n");
+                this.choix = 0;
+                this.attack++;
             }
-            if(choix!=2 && choix !=1 && choix!=0)
-                erreur("Saisie incorrecte ! ");
-            System.out.println("1. Tirer la prochaine carte ");
-            System.out.println("2. Quitter");
-            System.out.flush();
-
-            Scanner console = new Scanner(System.in);
-            choix = console.nextInt();
-            System.out.println("- -");
-            /*On retire les cartes*/
-            if(choix==1) {
-                if (tour % 2 != 0) {
-                    carteRetiree1 = this.joueur1.getPaquet().get(0);
-                    this.joueur1.tirer(carteRetiree1);
-                    System.out.println(carteRetiree1.toString() + "\n");
-                    choix = 0;
-                    attack++;
-                } else {
-                    carteRetiree2 = this.joueur2.getPaquet().get(0);
-                    this.joueur2.tirer(carteRetiree2);
-                    System.out.println(carteRetiree2.toString() + "\n");
-                    choix = 0;
-                    attack++;
+            carteRetiree1.setValeur(1);
+            carteRetiree2.setValeur(1);
+            /*On compare les cartes si les deux joueurs ont tiré une carte*/
+            if (this.attack % 2 == 0) {
+               
+                this.gagnerPoint(carteRetiree1,carteRetiree2);
+                /*bataille*/
+                if(carteRetiree1==carteRetiree2) {
+                    this.bataille(carteRetiree1,carteRetiree2);
                 }
-                /*On commence l'attaque si les deux joueurs ont tiré une carte*/
-                if (attack % 2 == 0) {
-                    carteRetiree1.setValeur(1);
-                    carteRetiree2.setValeur(1);
-                    this.gagnerPoint(carteRetiree1,carteRetiree2);
-
-                    /*bataille*/
-                   if(carteRetiree1==carteRetiree2) {
-                        this.bataille(carteRetiree1,carteRetiree2);
-                   }
-                }
-                tour++;
             }
-        }
-
-        /*Fin du jeu*/
-        if(choix==2) {
-            System.out.println("Fin de la partie");
+            this.tour++;
         }
     }
-
+    
     public void gagnerPoint(Carte c1, Carte c2){
         if (c1.superieur(c2)) {
             this.joueur1.ajouter(c1);
@@ -129,42 +127,34 @@ public class JeuPrincipal {
         ArrayList<Carte> stock = new ArrayList<Carte>();
         stock.add(c1);
         stock.add(c2);
-        int tour=1;
-        
+      
         /*tant que les cartes tirés des joueurs sont identiques, la bataille continue*/
-        while (c1.getValeur() == c2.getValeur() && tour%2!=0) {
-            if (tour % 2 != 0) {
-                System.out.println("C'est à " + this.joueur1.getPseudo() + " de jouer");
-            } else {
-                System.out.println("C'est à " + this.joueur2.getPseudo() + " de jouer");
-            }
-            System.out.println("1. Tirer la prochaine carte ");
-            System.out.flush();
-            Scanner console = new Scanner(System.in);
-            int choix2 = console.nextInt();
-            if (choix2 == 1) {
-                if (tour % 2 != 0) {
+        while (this.choix !=2 && this.choix!=1 && c1.getValeur() == c2.getValeur() && this.tour%2!=0) {
+            this.actionJoueur();
+            if (this.choix == 1) {
+                if (this.tour % 2 != 0) {
                     c1 = this.joueur1.getPaquet().get(0);
                     this.joueur1.tirer(c1);
                     System.out.println(c1.toString() + "\n");
                     stock.add(c1);
-                    choix2 = 0;
+                    this.choix = 0;
                 } else {
                     c2 = this.joueur2.getPaquet().get(0);
                     this.joueur2.tirer(c2);
                     System.out.println(c2.toString() + "\n");
                     stock.add(c1);
-                    choix2 = 0;
+                    this.choix = 0;
                     if(c2.getValeur()==c2.getValeur()) {
-                        tour++;
+                        this.tour++;
                     } else{
-                        tour=1;
+                        this.tour=1;
                     }
 
                 }
             }
         }
-                        /*Gagnant de la bataille*/
+        
+        /*Gagnant de la bataille*/
         if (c1.superieur(c2)) {
             System.out.println(this.joueur1.getPseudo() + " a gagné la bataille!");
             for (int i = 0; i < stock.size(); i++) {
@@ -172,12 +162,12 @@ public class JeuPrincipal {
             }
         } else {
             System.out.println(this.joueur1.getPseudo() + " a gagné la bataille!");
-            System.out.println(c1);
-            System.out.println(c2);
             for (int i = 0; i < stock.size(); i++) {
                 this.joueur1.ajouter(stock.get(i));
             }
         }
+        
+        this.finDuJeu();
     }
 
     public void afficherScore(){
@@ -195,6 +185,14 @@ public class JeuPrincipal {
     /*message erreur*/
     public void erreur(String libelle){
         System.out.println(libelle);
+    }
+    
+    /*La partie s'arrête si le joueur a fait le choix 2*/
+    public void finDuJeu(){
+         /*Fin du jeu*/
+        if(this.choix==2) {
+            System.out.println("Fin de la partie");
+        }
     }
 
 }
