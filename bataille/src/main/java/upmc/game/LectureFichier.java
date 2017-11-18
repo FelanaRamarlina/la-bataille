84efb302af0d9afd9f56ca2dbf402c3bb316f48d
FefeKimi <r.felana.gyal@gmail.com>
2017-11-18 18:18:57 +0100

    - Recherche de fichier saisie par l'utilisateur

--- bataille/src/main/java/upmc/game/LectureFichier.java
+++ bataille/src/main/java/upmc/game/LectureFichier.java
@@ -4,17 +4,46 @@
  * and open the template in the editor.
  */
 package upmc.game;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-/**
- *
- * @author Felana
- */
 public class LectureFichier implements LecturePseudo{
-
+    FileReader fileReader;
+    ArrayList<String> pseudos;
+    
+    public LectureFichier(){
+        this.pseudos = new ArrayList<String>();
+    }
+    public void demanderFichier(){
+        Scanner console = new Scanner(System.in);
+        System.out.print("Veuillez saisir le nom de fichier à lire : ");
+        String nomFichier = console.nextLine();
+        try{
+            File fichier = new File(nomFichier+".txt");
+            FileReader fileReader = new FileReader(fichier);
+            BufferedReader buffReader = new BufferedReader(fileReader);
+            BufferedReader br = new BufferedReader(new FileReader(fichier));
+            String line;
+            while ((line = br.readLine()) != null) {
+                this.pseudos.add(line+"\n");
+            }
+            buffReader.close();
+            fileReader.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Le fichier spécifié n'existe pas");
+        }
+        catch(IOException e){
+            System.out.println("Une erreur c'est produite lors de la lecture : "+e.getMessage());
+        }
+    }
     public ArrayList<String> lirePseudo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.pseudos;
     }
     
 }
